25b1823f0361ff8605671a97b6188dc121d6b5dd
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-10-14 22:20:14 +0800

    remove size check in romfs_get_file_by_hash
    Size-0 files should be conceptually permitted.

--- romfs.c
+++ romfs.c
@@ -68,7 +68,7 @@ static off_t romfs_seek(void * opaque, off_t offset, int whence) {
 const uint8_t * romfs_get_file_by_hash(const uint8_t * romfs, uint32_t h) {
     const uint8_t * meta;
 
-    for (meta = romfs; get_unaligned(meta) && get_unaligned(meta + 4); meta += get_unaligned(meta + 4) + 8) {
+    for (meta = romfs; get_unaligned(meta); meta += get_unaligned(meta + 4) + 8) {
         if (get_unaligned(meta) == h) {
             return meta + 8;
         }
