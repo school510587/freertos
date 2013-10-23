7b03f0f15313333312e01319eb6f97dcabbe6cdb
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-10-23 19:31:58 +0800

    Revert "add malloc/free into string-util.c"
    This reverts commit aae60304451836f88343282db9ebdcb467a70ebd.

--- string-util.c
+++ string-util.c
@@ -3,7 +3,6 @@
 #include <stdint.h>
 #include <stdarg.h>
 #include <limits.h>
-#include "FreeRTOS.h"
 #include "fio.h"
 
 #define ALIGN (sizeof(size_t))
@@ -109,16 +108,6 @@ int isalnum(int c)
 int isspace(int c)
 {
 	return (c == ' ');
-}
-
-void free(void *p)
-{
-	vPortFree(p);
-}
-
-void *malloc(size_t size)
-{
-	return pvPortMalloc(size);
 }
 
 void *memset(void *dest, int c, size_t n)
