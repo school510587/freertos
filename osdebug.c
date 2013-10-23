e3bf800c70c7d14e9483ea5b10fb717144371398
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-10-24 02:02:14 +0800

    rewrite prng() using inline assembly
    The structure refers to tim37021's naked attribute (rtenv also used it).
    Usage of registers refers to zzz0072's inline assembly.
    
    Issue: How to perform "and" operation with 0xFFFF constant?
    
    References:
    [1] http://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html
    [2] zzz0072/freertos/master
    [3] tim37021/freertos/master

--- osdebug.c
+++ osdebug.c
@@ -18,13 +18,26 @@ typedef struct {
 static unsigned int lfsr = 0xACE1;
 
 // Get a pseudorandom number generator from Wikipedia
+static int prng(void) __attribute__((naked));
 static int prng(void)
 {
-    static unsigned int bit;
     /* taps: 16 14 13 11; characteristic polynomial: x^16 + x^14 + x^13 + x^11 + 1 */
-    bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
-    lfsr =  (lfsr >> 1) | (bit << 15);
-    return lfsr & 0xffff;
+    __asm__ (
+        "mov r0, %1\t\n" // r0 = lfsr
+        "eor r1, r0, r0, lsr #2 \t\n" // r1 = (g_lfsr >> 0) ^ (g_lfsr >> 2)
+        "eor r1, r1, r0, lsr #3 \t\n" // r1 = r1 ^ (g_lfsr >> 3)
+        "eor r1, r1, r0, lsr #5 \t\n" // r1 = r1 ^ (g_lfsr >> 5)
+        "and r1, r1, #1         \t\n" // bit = r1 = r1 & 1
+        "lsl r1, r1, #15        \t\n" // r1 = bit << 15
+        "orr r0, r1, r0, lsr #1 \t\n" // lfsr = r1 | (lfsr >> 1)
+        "movw r1, #0xFFFF       \t\n"
+        "and r0, r0, r1         \t\n" // r0 = r1 & 0xffff
+        "mov %0, r0             \t\n"
+        : "=r" (lfsr)
+        : "r" (lfsr)
+        : "r0", "r1"
+    );
+    __asm__ ("bx lr \t\n");
 }
 
 static mmtest_slot read_cb(const mmtest_slot *slots, unsigned int *read_pointer, unsigned int write_pointer)
