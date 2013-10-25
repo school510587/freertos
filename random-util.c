81560a2723c5296cab3912fdd6a57b7dc90a2982
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-10-25 12:54:39 +0800

    rewrite rand() using inline assembly
    The structure refers to tim37021's naked attribute (rtenv also used it).
    Usage of registers refers to zzz0072's inline assembly.
    
    References:
    [1] http://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html
    [2] http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0344c/Beiiegaf.html
    [3] zzz0072/freertos/master
    [4] tim37021/freertos/master

--- random-util.c
+++ random-util.c
@@ -4,12 +4,24 @@
 static unsigned int state = 0xACE1;
 
 // Get a pseudorandom number generator from Wikipedia
+int rand() __attribute__((naked));
 int rand()
 {
     /* taps: 16 14 13 11; characteristic polynomial: x^16 + x^14 + x^13 + x^11 + 1 */
-    unsigned int bit = ((state >> 0) ^ (state >> 2) ^ (state >> 3) ^ (state >> 5) ) & 1;
-    state = (state >> 1) | (bit << 15);
-    return (int)state;
+    __asm__ (
+        "mov r0, %1\t\n" // r0 = state
+        "eor r1, r0, r0, lsr #2 \t\n" // r1 = (g_state >> 0) ^ (g_state >> 2)
+        "eor r1, r1, r0, lsr #3 \t\n" // r1 = r1 ^ (g_state >> 3)
+        "eor r1, r1, r0, lsr #5 \t\n" // r1 = r1 ^ (g_state >> 5)
+        "and r1, r1, #1         \t\n" // bit = r1 = r1 & 1
+        "lsl r1, r1, #15        \t\n" // r1 = bit << 15
+        "orr r0, r1, r0, lsr #1 \t\n" // state = r1 | (state >> 1)
+        "mov %0, r0             \t\n" // output
+        : "=r" (state)
+        : "r" (state)
+        : "r0", "r1"
+    );
+    __asm__ ("bx lr \t\n");
 }
 
 // Set the state of pseudorandom number generator
